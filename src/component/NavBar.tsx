9ad135316f6bf44b5e6988163b31e78d25eaf41a
Rohanaow123 <Rohan@aow.co.in>
2025-08-26 11:02:41 +0530

    update

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -44,7 +44,6 @@ const navItems: NavItem[] = [
   { title: "Prices", href: "/prices" },
   { title: "FAQ", href: "/FAQs" },
   { title: "Contacts", href: "/contact" },
-  { title: "Track", href: "/tracking" }, // FIX: This line ensures the link points to the correct URL path "/track".
 ]
 
 const Navbar = () => {
